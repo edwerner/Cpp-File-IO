2fa462215a4e3b2256df3a02ae5cfc0944ba8f6d
Ed Werner <moshmartyr@gmail.com>
2012-06-13 01:03:25 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int
+         
+main ( void ) {       
+
+	int a;
+	cout << "Enter a number: ";	
+	cin >> a;
+	cout << "You entered: " << a << "!" << endl;
+	return 0;
+	
+}
